683b62bd0c767f5cd725704db9693569b4b08b44
Tomas Tulka <tomas.tulka@gmail.com>
2020-04-10 16:18:31 +0200

    better url parsing

--- src/common/linkify.js
+++ src/common/linkify.js
@@ -1,6 +1,6 @@
 export default function(text) {
     if (text) {
-        return text.replace(/(https?:\/\/[^\s]+)/g,
+        return text.replace(/(https?:\/\/[^\s)("<>]+)/g,
                             url => '<a href="' + url + '" target="_blank" rel="noopener noreferrer">' + url + '</a>');
     }
 }
